6499ab1b1958cde34786909f84f7548d09bed95d
Laura White <law7676@outlook.com>
2022-04-19 15:47:19 -0500

    Create map-2-exercise.js

---
+++ js-fundamentals-to-functional/map-2-exercise.js
@@ -0,0 +1,49 @@
+// Implement ._map() as weith ._each(). Can use a loop or ._each()
+
+/*
+
+callbackfn should be a function that accepts three arguments. map calls callbackfn once for each element in the array, in ascending order, and constructs a new Array from the results. callbackfn is called only for elements of the array which actually exist; it is not called for missing elements of the array.
+
+If a thisArg parameter is provided, it will be used as the this value for each invocation of callbackfn. If it is not provided, undefined is used instead.
+
+callbackfn is called with three arguments: the value of the element, the index of the element, and the object being traversed.
+
+map does not directly mutate the object on which it is called but the object may be mutated by the calls to callbackfn.
+
+The range of elements processed by map is set before the first call to callbackfn. Elements which are appended to the array after the call to map begins will not be visited by callbackfn. If existing elements of the array are changed, their value as passed to callbackfn will be the value at the time map visits them; elements that are deleted after the call to map begins and before being visited are not visited.
+*/
+
+const _ = {};
+
+const newList = [];
+const newObj = {};
+
+const weapons = ["candlestick", "lead pipe", "revolver"];
+
+const weaponsObj = {
+    weapon1: "candlestick",
+    weapon2: "lead pipe",
+    weapon3: "revolver"
+}
+
+const makeBroken = function(item){
+    return `broken ${item}`
+}
+
+
+function map(list, callback) {
+    // check to see if item is an array
+    if (Array.isArray(list)){
+        // loop through the list     
+        for (var i = 0; i < list.length; i++){
+            // call the callback with a list item
+            newList.push(callback(list[i], i, list));
+        }
+    } /*else {
+        for (let key in list){
+            callback(list[key], key, list)
+       }
+    }*/
+    return newList;
+}
+console.log(map(weapons, makeBroken));
