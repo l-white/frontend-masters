6d18759beee154d84f15fcb22316edf1cb5ad34f
Laura White <law7676@outlook.com>
2022-04-19 10:59:33 -0500

    Update each-exercise.js

--- js-fundamentals-to-functional/each-exercise.js
+++ js-fundamentals-to-functional/each-exercise.js
@@ -4,12 +4,31 @@
 
 // Test this function by running it with the code in the foreach.js file
 
-_.each = function(list, callback){
-  // Todo
-}
+  /*
+  _.each(suspects, function(name) {
+    let suspectObj = createSuspectObjects(name);
+    suspectsList.push(suspectObj);
+  })*/
 
-/*
-_.each(suspects, function(name) {
-  let suspectObj = createSuspectObjects(name);
-  suspectsList.push(suspectObj);
-})*/
+const suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White"];
+
+const clueGame = {
+    characters: ["Miss Scarlet", "Mr. Green", "Colonel Mustard", "Professor Plum", "Mrs. Peacock", "Mrs. White"],
+    weapons: ["candlestick", "knife", "lead pipe", "revolver", "rope", "wrench"],
+    rooms: ["kitchen", "ballroom", "conservatory", "dining room", "cellar", "billard room", "library", "lounge", "hall", "study"],
+    confidential: []
+  }
+
+function each(list, callback) {
+    if (Array.isArray(list) === true) {
+        for (let i = 0; i < list.length; i++){
+            callback(list[i])
+        }
+    } else if (typeof list === "object") {
+        for (let key in clueGame){
+            console.log(clueGame[key])
+          }
+    } else {
+        console.log("Not an array or an object");
+    }
+}
