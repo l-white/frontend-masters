d5879184ead4a997d0141a9d3daf63c37266e189
Laura White <law7676@outlook.com>
2022-04-19 15:46:06 -0500

    Update each-exercise.js

--- js-fundamentals-to-functional/each-exercise.js
+++ js-fundamentals-to-functional/each-exercise.js
@@ -4,12 +4,6 @@
 
 // Test this function by running it with the code in the foreach.js file
 
-  /*
-  _.each(suspects, function(name) {
-    let suspectObj = createSuspectObjects(name);
-    suspectsList.push(suspectObj);
-  })*/
-
 const suspects = ["Miss Scarlet", "Colonel Mustard", "Mr. White"];
 
 const clueGame = {
@@ -22,13 +16,32 @@ const clueGame = {
 function each(list, callback) {
     if (Array.isArray(list) === true) {
         for (let i = 0; i < list.length; i++){
-            callback(list[i])
+            callback(list[i], i, list)
         }
     } else if (typeof list === "object") {
-        for (let key in clueGame){
-            console.log(clueGame[key])
+        for (let key in list){
+            callback(list[key], key, list)
           }
     } else {
         console.log("Not an array or an object");
     }
 }
+
+console.log(each(suspects, console.log))
+
+each(['sally', 'georgie', 'anna'], function(name, i, list){
+    if (list[i + 1]) {
+        console.log(name, 'is younger than', list[i + 1] + ".")
+    } else {
+        console.log(name, 'is the oldest.')
+    }
+    
+})
+console.log(each(clueGame, console.log))
+
+  
+  /*
+  _.each(suspects, function(name) {
+    let suspectObj = createSuspectObjects(name);
+    suspectsList.push(suspectObj);
+  })*/
