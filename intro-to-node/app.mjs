40c1530eea34aba574dc2c28af240cdfa57bc3c7
Laura <law7676@outlook.com>
2022-07-29 19:07:25 -0500

    added intro-to-node

---
+++ intro-to-node/app.mjs
@@ -0,0 +1,8 @@
+// using common js
+//const action = require('./index')
+
+// using mjs
+// need to wrap in brackets because it's named export
+import {action} from './index.mjs'
+
+action()
---
+++ intro-to-node/index.mjs
@@ -0,0 +1,14 @@
+// exports not included will be private to this file
+
+// export with common js
+/*
+const action = () => {
+  console.log('hello')
+}
+module.exports = action
+*/
+
+// export with mjs - ES6 Syntax
+export const action = () => {
+  console.log('hello')
+}
